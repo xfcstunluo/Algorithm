b737f65c392b93efe34c44bbe402544b4ac9c942
yuzeyu <2602316693@qq.com>
2025-09-04 15:03:35 +0800

    算法

---
+++ src/HUAWEI24/HUAWEI101602.java
@@ -0,0 +1,7 @@
+package HUAWEI24;
+
+public class HUAWEI101602 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Sort/QuickSort.java
@@ -0,0 +1,34 @@
+package Sort;
+
+public class QuickSort {
+    public static void main(String[] args){
+        int[] arr = {5, 2, 9, 1, 6, 3};
+        quickSort(arr,0,arr.length-1);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+    public static void quickSort(int[] nums,int left,int right){
+        if(left>=right) return;
+        int pivot=patition(nums,left,right);
+        quickSort(nums,left,pivot-1);
+        quickSort(nums,pivot+1,right);
+    }
+    public static int patition(int[] nums,int left,int right){
+        int i=left+1,j=right;
+        while(i<=j){
+            while (i<=right&&nums[left]>=nums[i]) i++;
+            while(j>=left+1&&nums[left]<=nums[j]) j--;
+            if(i<j) swap(nums,i,j);
+        }
+        swap(nums,left,j);
+        return j;
+
+    }
+    public static void swap(int[] nums,int i,int j){
+        int tmp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=tmp;
+    }
+}
+
